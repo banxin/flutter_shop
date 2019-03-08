88e15a5b61d92cf21f6569d7f16e54707eabc4d7
山竹 <lang.yan@hipac.cn>
2019-03-08 11:48:31 +0800

    简单的get，post 请求，以及伪造请求头获取真实数据

---
+++ lib/config/http_headers.dart
@@ -0,0 +1,11 @@
+const httpHeaders = {
+  'Accept': 'application/json, text/plain, */*',
+'Accept-Encoding': 'gzip, deflate, br',
+'Accept-Language': 'zh-CN,zh;q=0.9',
+'Connection': 'keep-alive',
+'Cookie': '_ga=GA1.2.2097779540.1552015488; _gid=GA1.2.1662865434.1552015488; SERVERID=3431a294a18c59fc8f5805662e2bd51e|1552016529|1552015485; _gat=1; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1552015487,1552015545,1552016530; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1552016530',
+'Host': 'time.geekbang.org',
+'Origin': 'https://time.geekbang.org',
+'Referer': 'https://time.geekbang.org/',
+'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+};
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,12 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_shop/config/http_headers.dart';
 
-class CategoryPage extends StatelessWidget {
+// class CategoryPage extends StatelessWidget {
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//     return Center(
+//       child: Text('分类'),
+//     );
+//   }
+// }
+
+class CategoryPage extends StatefulWidget {
+  final Widget child;
+
+  CategoryPage({Key key, this.child}) : super(key: key);
+
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+
+  String showText = '还没有请求数据';
 
   @override
   Widget build(BuildContext context) {
-
-    return Center(
-      child: Text('分类'),
+    return Container(
+       child: Scaffold(
+         appBar: AppBar(title: Text('远程数据'),),
+        body: SingleChildScrollView( // 滚动控件
+          child: Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: (){
+                  _jike();
+                },
+                child: Text('请求数据'),
+              ),
+              Text(showText),
+            ],
+          ),
+        ),
+       ),
     );
+  }
+
+  void _jike() {
+
+    print('开始向极客时间请求数据');
+
+    _getHttp().then((val) {
+      setState(() {
+        showText = val['data'].toString();
+      });
+    });
+  }
+
+  Future _getHttp() async {
+    try {
+      Response response;
+      Dio dio = new Dio();
+
+      dio.options.headers = httpHeaders;
+
+      response = await dio.get('https://time.geekbang.org/serv/v1/column/newAll');
+      print(response);
+      return response.data;
+    } catch (e) {
+      return print(e);
+    }
   }
 }
